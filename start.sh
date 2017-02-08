9976107e147cef499f8d9d4d5d90879e0e78e25c
mezz64 <jtmihalic@gmail.com>
2017-02-08 11:19:14 -0500

    some user fixes for unraid

--- start.sh
+++ start.sh
@@ -3,9 +3,18 @@
 #Give message when starting the container
 printf "\n \n \n ------------------------Starting container ------------------------ \n \n \n"
 
+# Configure user nobody to match unRAID's settings
+#export DEBIAN_FRONTEND="noninteractive"
+usermod -u 99 nobody
+usermod -g 100 nobody
+usermod -d /home nobody
+chown -R nobody:users /home
+
 cp /converteac3.sh /config/converteac3.sh
+chown -R nobody:users /config
 
 echo "[Info] Starting script"
-bash /config/converteac3.sh
+#bash /config/converteac3.sh
+su - nobody -c /config/converteac3.sh
 
 echo "Stopping Container, script finished.."
