eb362d4f041a406b23779aad1230a4ae9023b953
mezz64 <jtmihalic@gmail.com>
2017-02-07 23:03:17 -0500

    should really test this locally first...

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 #Give message when starting the container
 printf "\n \n \n ------------------------Starting container ------------------------ \n \n \n"
 
+cp /converteac3.sh /config/converteac3.sh
+
 echo "[Info] Starting script"
 bash /config/converteac3.sh
 
